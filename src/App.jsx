799251f7b305be12f14f7c452c36231bb51443aa
KyoDeath <ethangenevriez@gmail.com>
2024-03-09 00:49:07 +0100

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Catalogue from './components/Catalogue';
 import Layout from './components/Layout';
 import Panier from './components/Panier';
 import AboutUs from './components/AboutUs';
+import ProductsDetails from "./components/ProductsDetails.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
    <Routes>
     <Route path="/" element={<Layout/>}>
       <Route index element={<Catalogue/>}></Route>
+      <Route path="/product/:id" element={<ProductsDetails />} />
       <Route path="panier" element={<Panier/>}/>
       <Route path="aboutus" element={<AboutUs/>}/>
     </Route>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faCartShopping, faClose, faUser} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
+
   return (
-    <div className=''>
-    <span>Home</span>
-    <span>Catalogue </span>
-    <span>About Us</span>
-    </div>
+      <header className="bg-white">
+        <nav className="flex justify-between items-center w-[92%] mx-auto">
+          <div className="z-20">
+            <Link className="text-2xl" to="/">E-Commerce</Link>
+          </div>
+          <div className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[40vh] left-0 ${toggle ? 'top-0' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}>
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+              <li>
+                <Link className="hover:text-gray-500" to="/">Home</Link>
+              </li>
+              <li>
+                <Link className="hover:text-gray-500" to="/aboutus">About Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="z-20 flex items-center gap-6">
+            <Link to="/panier"><FontAwesomeIcon icon={faCartShopping}/></Link>
+            <Link to="/profile"><FontAwesomeIcon icon={faUser}/></Link>
+            <div className="text-3xl cursor-pointer md:hidden" onClick={handleToggle}>
+              {toggle?<FontAwesomeIcon icon={faClose} />:<FontAwesomeIcon icon={faBars}/>}
+            </div>
+
+          </div>
+        </nav>
+      </header>
   )
 };
 
